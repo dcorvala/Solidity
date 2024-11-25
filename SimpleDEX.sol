09e97a3e9acc9762c03028587636b79df554c2a5
Dario <131714965+dcorvala@users.noreply.github.com>
2024-11-25 10:04:40 -0300

    Update SimpleDEX.sol

--- SimpleDEX.sol
+++ SimpleDEX.sol
@@ -1,1 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SimpleDEX {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+
+    uint256 public reserveA;
+    uint256 public reserveB;
+
+    event LiquidityAdded(address indexed provider, uint256 amountA, uint256 amountB);
+    event LiquidityRemoved(address indexed provider, uint256 amountA, uint256 amountB);
+    event TokensSwapped(address indexed swapper, address inputToken, address outputToken, uint256 inputAmount, uint256 outputAmount);
+
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+    }
+
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Amounts must be greater than zero");
+
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        reserveA += amountA;
+        reserveB += amountB;
+
+        emit LiquidityAdded(msg.sender, amountA, amountB);
+    }
+
+    function removeLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Amounts must be greater than zero");
+        require(amountA <= reserveA && amountB <= reserveB, "Not enough liquidity");
+
+        reserveA -= amountA;
+        reserveB -= amountB;
+
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+
+        emit LiquidityRemoved(msg.sender, amountA, amountB);
+    }
+
+    function swapAforB(uint256 amountAIn) external {
+        require(amountAIn > 0, "Amount must be greater than zero");
+
+        uint256 amountBOut = getSwapAmount(amountAIn, reserveA, reserveB);
+        require(amountBOut > 0, "Invalid swap");
+
+        tokenA.transferFrom(msg.sender, address(this), amountAIn);
+        tokenB.transfer(msg.sender, amountBOut);
+
+        reserveA += amountAIn;
+        reserveB -= amountBOut;
+
+        emit TokensSwapped(msg.sender, address(tokenA), address(tokenB), amountAIn, amountBOut);
+    }
+
+    function swapBforA(uint256 amountBIn) external {
+        require(amountBIn > 0, "Amount must be greater than zero");
+
+        uint256 amountAOut = getSwapAmount(amountBIn, reserveB, reserveA);
+        require(amountAOut > 0, "Invalid swap");
+
+        tokenB.transferFrom(msg.sender, address(this), amountBIn);
+        tokenA.transfer(msg.sender, amountAOut);
+
+        reserveB += amountBIn;
+        reserveA -= amountAOut;
+
+        emit TokensSwapped(msg.sender, address(tokenB), address(tokenA), amountBIn, amountAOut);
+    }
+
+    function getPrice(address _token) external view returns (uint256) {
+        require(_token == address(tokenA) || _token == address(tokenB), "Invalid token");
+
+        if (_token == address(tokenA)) {
+            return (reserveB * 1e18) / reserveA;
+        } else {
+            return (reserveA * 1e18) / reserveB;
+        }
+    }
+
+    function getSwapAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) internal pure returns (uint256) {
+        uint256 inputAmountWithFee = inputAmount * 997; // Fee: 0.3%
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 1000) + inputAmountWithFee;
+        return numerator / denominator;
+    }
+}
