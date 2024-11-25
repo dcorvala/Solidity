de64ca0fa56adfc6c0320eb52a4b7f2e45e3cf68
Dario <131714965+dcorvala@users.noreply.github.com>
2024-11-25 10:05:56 -0300

    Update SimpleDEX_flattened.sol

--- SimpleDEX_flattened.sol
+++ SimpleDEX_flattened.sol
@@ -1,1 +1,177 @@
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
 
+
+// OpenZeppelin Contracts (last updated v5.1.0) (token/ERC20/IERC20.sol)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+//pragma solidity ^0.8.20;
+
+/**
+ * @dev Interface of the ERC-20 standard as defined in the ERC.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool);
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+// File: Exchange_Descentralizado/Trabajo_Final_3/SimpleDEX.sol
+
+
+//pragma solidity ^0.8.0;
+
+
+contract SimpleDEX {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+
+    uint256 public reserveA;
+    uint256 public reserveB;
+
+    event LiquidityAdded(address indexed provider, uint256 amountA, uint256 amountB);
+    event LiquidityRemoved(address indexed provider, uint256 amountA, uint256 amountB);
+    event TokensSwapped(address indexed swapper, address inputToken, address outputToken, uint256 inputAmount, uint256 outputAmount);
+
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+    }
+
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Amounts must be greater than zero");
+
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        reserveA += amountA;
+        reserveB += amountB;
+
+        emit LiquidityAdded(msg.sender, amountA, amountB);
+    }
+
+    function removeLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Amounts must be greater than zero");
+        require(amountA <= reserveA && amountB <= reserveB, "Not enough liquidity");
+
+        reserveA -= amountA;
+        reserveB -= amountB;
+
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+
+        emit LiquidityRemoved(msg.sender, amountA, amountB);
+    }
+
+    function swapAforB(uint256 amountAIn) external {
+        require(amountAIn > 0, "Amount must be greater than zero");
+
+        uint256 amountBOut = getSwapAmount(amountAIn, reserveA, reserveB);
+        require(amountBOut > 0, "Invalid swap");
+
+        tokenA.transferFrom(msg.sender, address(this), amountAIn);
+        tokenB.transfer(msg.sender, amountBOut);
+
+        reserveA += amountAIn;
+        reserveB -= amountBOut;
+
+        emit TokensSwapped(msg.sender, address(tokenA), address(tokenB), amountAIn, amountBOut);
+    }
+
+    function swapBforA(uint256 amountBIn) external {
+        require(amountBIn > 0, "Amount must be greater than zero");
+
+        uint256 amountAOut = getSwapAmount(amountBIn, reserveB, reserveA);
+        require(amountAOut > 0, "Invalid swap");
+
+        tokenB.transferFrom(msg.sender, address(this), amountBIn);
+        tokenA.transfer(msg.sender, amountAOut);
+
+        reserveB += amountBIn;
+        reserveA -= amountAOut;
+
+        emit TokensSwapped(msg.sender, address(tokenB), address(tokenA), amountBIn, amountAOut);
+    }
+
+    function getPrice(address _token) external view returns (uint256) {
+        require(_token == address(tokenA) || _token == address(tokenB), "Invalid token");
+
+        if (_token == address(tokenA)) {
+            return (reserveB * 1e18) / reserveA;
+        } else {
+            return (reserveA * 1e18) / reserveB;
+        }
+    }
+
+    function getSwapAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) internal pure returns (uint256) {
+        uint256 inputAmountWithFee = inputAmount * 997; // Fee: 0.3%
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 1000) + inputAmountWithFee;
+        return numerator / denominator;
+    }
+}
